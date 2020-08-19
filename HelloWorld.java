eb4894484e394c1e6437264daa90921f503af133
chenrq335 <43854955+chenrq335@users.noreply.github.com>
2020-08-19 10:01:41 +0800

    hello world第一次提交

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package com.szy.entity;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("say hellowold!");
+
+	}
+
+}
